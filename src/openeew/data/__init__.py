5089c8dd1d1eeaec217e148d26b202b77032048b
Michael Allman <michael@grillo.io>
2019-04-15 08:23:04 -0500

    added docstring

--- src/openeew/data/__init__.py
+++ src/openeew/data/__init__.py
@@ -13,3 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =============================================================================
+"""
+This module provides a means to work with OpenEEW data.
+"""
